2ba8db8f77f4d0dd87d83443d1113f87ecef0568
esakkiammal23 <snowmoon2302gmail.com>
2026-07-21 14:58:45 +0530

    Remove debug prints

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -103,12 +103,7 @@ DATABASES = {
     }
 }
 
-print("=== DATABASE CONFIG ===")
-print("DB_NAME:", os.getenv("DB_NAME"))
-print("DB_HOST:", os.getenv("DB_HOST"))
-print("DB_PORT:", os.getenv("DB_PORT"))
-print("DB_USER:", os.getenv("DB_USER"))
-print("=======================")
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
